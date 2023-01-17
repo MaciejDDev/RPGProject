6ab4065f81046c3612d9a47a9fb269449fcf8f59
MaciejDDev <114737615+MaciejDDev@users.noreply.github.com>
2023-01-17 18:29:33 +0100

    Created basic boolean flag system

---
+++ GameArchitectureCourseRPG/Assets/Scripts/Flags/GameFlag.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(menuName ="Bool Game Flag")]
+public class GameFlag : ScriptableObject
+{
+    public bool Value;
+
+    void OnEnable() => Value = default;
+}
