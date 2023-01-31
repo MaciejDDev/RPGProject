37ec85e27407f7a7068ea001fef3b5b0d6074d23
MaciejDDev <114737615+MaciejDDev@users.noreply.github.com>
2023-01-31 17:28:48 +0100

    Added metIntFlag condition

---
+++ GameArchitectureCourseRPG/Assets/Scripts/Inspection/MetIntFlagCondition.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MetIntFlagCondition : MonoBehaviour, IMet
+{
+
+    [SerializeField] IntGameFlag _requiredFlag;
+    [SerializeField] int _requiredValue;
+
+    public bool Met() => _requiredFlag.Value >= _requiredValue;
+}
