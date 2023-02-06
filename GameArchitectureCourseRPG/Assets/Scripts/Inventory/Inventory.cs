d3c56f5525bf855ccd7fd07e74a5c2c5fc110c43
MaciejDDev <114737615+MaciejDDev@users.noreply.github.com>
2023-02-06 17:47:33 +0100

    Added Item Slots

--- GameArchitectureCourseRPG/Assets/Scripts/Inventory/Inventory.cs
+++ GameArchitectureCourseRPG/Assets/Scripts/Inventory/Inventory.cs
@@ -1,19 +1,30 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public List<Item> _items = new List<Item>();
+
+    const int GENERAL_SIZE = 9;
+    public ItemSlot[] GeneralInventory = new ItemSlot[GENERAL_SIZE];
+
     [SerializeField] Item _debugItem;
 
+
+    private void Awake()
+    {
+        for (int i = 0; i < GENERAL_SIZE; i++)
+        {
+            GeneralInventory[i] = new ItemSlot();
+        }
+    }
     public void AddItem(Item item)
     {
-        _items.Add(item);
+        var firstAvailableSlot = GeneralInventory.FirstOrDefault(t => t.IsEmpty);
+        firstAvailableSlot.SetItem(item);
     }
     [ContextMenu(nameof(AddDebugItem))]
     void AddDebugItem() => AddItem(_debugItem);
 
 }
-    
