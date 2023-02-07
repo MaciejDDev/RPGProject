54e5e0544a463e6218a447f34597fdcc9a79342f
MaciejDDev <114737615+MaciejDDev@users.noreply.github.com>
2023-02-07 17:45:12 +0100

    Added dragging and dropping to inventory slots

--- GameArchitectureCourseRPG/Assets/Scripts/UI/InventoryPanelSlot.cs
+++ GameArchitectureCourseRPG/Assets/Scripts/UI/InventoryPanelSlot.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventoryPanelSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class InventoryPanelSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     ItemSlot _itemSlot;
+    [SerializeField] Image _draggedItemIcon;
     [SerializeField] Image _itemIcon;
     [SerializeField] Outline _outline;
+    [SerializeField] Color _draggingColor = Color.gray;
 
     public void Bind(ItemSlot itemSlot)
     {
@@ -24,6 +26,29 @@ public class InventoryPanelSlot : MonoBehaviour, IPointerEnterHandler, IPointerE
     public void OnPointerExit(PointerEventData eventData)
     {
         _outline.enabled = false;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if(_itemSlot.IsEmpty) 
+            return;
+
+        _itemIcon.color = _draggingColor;
+        _draggedItemIcon.sprite = _itemIcon.sprite;
+        _draggedItemIcon.enabled = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _draggedItemIcon.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _itemIcon.color = Color.white;
+        _draggedItemIcon.sprite = null;
+        _draggedItemIcon.enabled = false;
+
     }
 
     private void UpdateIcon()
