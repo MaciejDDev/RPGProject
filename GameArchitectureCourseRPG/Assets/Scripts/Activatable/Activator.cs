b49505595082704f6a012ca9cecfa765342a5e2e
MaciejDDev <114737615+MaciejDDev@users.noreply.github.com>
2023-02-22 18:35:07 +0100

    Activator supports range & tag selection

--- GameArchitectureCourseRPG/Assets/Scripts/Activatable/Activator.cs
+++ GameArchitectureCourseRPG/Assets/Scripts/Activatable/Activator.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Activator : MonoBehaviour
 {
+    [SerializeField] float _radius = 10f;
+    [SerializeField] string _activatableTag;
+
     public void Activate()
     {
-        var allActivatables = FindObjectsOfType<Activatable>();
-        foreach (var activatable in allActivatables)
+
+        var allActivatablesMatchingTag = FindObjectsOfType<Activatable>().
+            Where(t => t.CompareTag(_activatableTag));
+
+
+        var allActivatablesInRange = allActivatablesMatchingTag.
+            Where(t => Vector3.Distance(transform.position, t.transform.position) <= _radius);
+        foreach (var activatable in allActivatablesInRange)
         {
             activatable.Toggle();
         }
