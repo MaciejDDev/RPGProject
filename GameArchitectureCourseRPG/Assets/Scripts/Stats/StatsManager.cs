ace196a45d987d4723bbdbd26f2e6bc7daeaa8b8
MaciejDDev <114737615+MaciejDDev@users.noreply.github.com>
2023-03-02 19:01:10 +0100

    Added Salvaging Stat & started Implementing Stats Dictionary

---
+++ GameArchitectureCourseRPG/Assets/Scripts/Stats/StatsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsManager : MonoBehaviour
+{
+    [SerializeField]  Stat[] _allStats;
+
+    Dictionary<Stat, int> _myStats = new Dictionary<Stat, int>();
+
+    
+    
+    
+    public static StatsManager Instance { get; private set; }
+    void OnValidate() => _allStats = Extensions.GetAllInstances<Stat>();
+    void Awake()
+    {
+        Instance = this;
+        foreach (Stat stat in _allStats)
+        {
+            _myStats.Add(stat, stat.DefaultValue);
+        }
+    }
+
+    public int GetStatValue(Stat stat)
+    {
+        if (_myStats.TryGetValue(stat, out int value)) 
+            return value;
+        
+        return 0;
+    }
+
+}
