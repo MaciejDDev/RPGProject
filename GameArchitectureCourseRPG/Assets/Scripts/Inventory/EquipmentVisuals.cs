1c0c36c9b5738bee6177b99a807f86c9d19b2dc6
MaciejDDev <114737615+MaciejDDev@users.noreply.github.com>
2023-03-14 18:37:03 +0100

    Added primary weapon (Blaster) & Added weapon visual model swapping

--- GameArchitectureCourseRPG/Assets/Scripts/Inventory/EquipmentVisuals.cs
+++ GameArchitectureCourseRPG/Assets/Scripts/Inventory/EquipmentVisuals.cs
@@ -12,12 +12,13 @@ public class EquipmentVisuals : MonoBehaviour
 
     void Start()
     {
+        //yield return null;
         foreach (var visual in Visuals)
             visual.DefaultMaterial = visual.SkinnedMeshRenderers.FirstOrDefault()?.material;
         
         foreach (var slot in Inventory.Instance.EquipmentSlots)
         {
-            slot.Changed += () =>
+            slot.Changed += () => UpdateEquipmentVisual(slot);
             UpdateEquipmentVisual(slot);
         }
     }
@@ -27,9 +28,22 @@ public class EquipmentVisuals : MonoBehaviour
         foreach (var visual in Visuals.Where(t => t.EquipmentSlotType == slot.EquipmentSlotType))
         {
             foreach (var skinnedMeshRenderer in visual.SkinnedMeshRenderers)
-            {
                 skinnedMeshRenderer.material = slot.Item?.EquipMaterial ?? visual.DefaultMaterial;
-            }
+            
+
+                //Debug.Log($"Visual Root: { visual.VisualModelRoot} ");
+                if (visual.VisualModelRoot != null)
+                {
+                    for (int i = 0; i < visual.VisualModelRoot.childCount; i++)
+                    {
+                        var model = visual.VisualModelRoot.GetChild(i);
+                        model.gameObject.SetActive(model.name == slot.Item?.ModelName);
+                        //Debug.LogError("Setting active/inactive weapons");
+                    }
+                }
+            
+            
+            
         }
     }
 }
@@ -42,4 +56,5 @@ public class EquipmentVisual
     public EquipmentSlotType EquipmentSlotType;
     public List<SkinnedMeshRenderer> SkinnedMeshRenderers;
     public Material DefaultMaterial;
+    public Transform VisualModelRoot;
 }
--- GameArchitectureCourseRPG/Assets/Scripts/Inventory/Item.cs
+++ GameArchitectureCourseRPG/Assets/Scripts/Inventory/Item.cs
@@ -10,6 +10,7 @@ public class Item : ScriptableObject
 
     public EquipmentSlotType EquipmentSlotType;
     public Material EquipMaterial;
+    public string ModelName;
     public Sprite Icon;
 
     [Multiline(lines:3)]
