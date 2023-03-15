a2a27dfc4f5572a09903a6db3199a55b222c86c1
MaciejDDev <114737615+MaciejDDev@users.noreply.github.com>
2023-03-15 18:55:41 +0100

    Added StatMods to Item Tooltip

--- GameArchitectureCourseRPG/Assets/Scripts/UI/ItemTooltipPanel.cs
+++ GameArchitectureCourseRPG/Assets/Scripts/UI/ItemTooltipPanel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -10,6 +11,7 @@ public class ItemTooltipPanel : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] TMP_Text _name;
     [SerializeField] TMP_Text _description;
+    [SerializeField] TMP_Text _stats;
     [SerializeField] Image _icon;
     [SerializeField] Button _placeButton;
 
@@ -47,9 +49,23 @@ public class ItemTooltipPanel : MonoBehaviour, IPointerClickHandler
             Toggle(true);
             _name.SetText(item.name);
             _description.SetText(item.Description);
+            _stats.SetText(GetStatsText(item));
             _icon.sprite= item.Icon;
             _placeButton.gameObject.SetActive(item.PlaceablePrefab != null);
         }
+    }
+
+    string GetStatsText(Item item)
+    {
+        StringBuilder b = new StringBuilder();
+        foreach (var statMod in item.StatMods)
+        {
+            if ( statMod.Value > 0)
+                b.AppendLine($"+{statMod.Value} {statMod.StatType.name}");
+            else
+                b.AppendLine($"-{statMod.Value} {statMod.StatType.name}");
+        }
+        return b.ToString();
     }
 
     void Toggle(bool visible)
